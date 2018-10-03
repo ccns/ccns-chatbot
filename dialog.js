498e27b8960d36ea24cf1a027facff8e4c47e4a9
HexRabbit <mikuqb8718@gmail.com>
2018-10-04 01:10:10 +0800

    Add 'utf8' option to avoid toString() method

--- dialog.js
+++ dialog.js
@@ -1,9 +1,9 @@
 const fs = require("fs")
 
 const data_dir = "data/"
-var welcome_msg = fs.readFileSync(data_dir+"welcome.txt").toString()
-var help_msg = fs.readFileSync(data_dir+"help.txt").toString()
-var chatroom_msg = fs.readFileSync(data_dir+"chatroom.txt").toString()
+var welcome_msg = fs.readFileSync(data_dir+"welcome.txt", 'utf8')
+var help_msg = fs.readFileSync(data_dir+"help.txt", 'utf8')
+var chatroom_msg = fs.readFileSync(data_dir+"chatroom.txt", 'utf8')
 
 module.exports = {
   GetChatroom: () => chatroom_msg,
